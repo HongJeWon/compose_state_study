16445257c86e0dcfd616b561ffee7d70db46df33
DevJeWon <dev.jewon@gmail.com>
2022-11-22 13:21:26 +0900

    set Wellness

--- app/src/main/java/com/example/basicstatemycode/MainActivity.kt
+++ app/src/main/java/com/example/basicstatemycode/MainActivity.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basicstatemycode.ui.theme.BasicStateMyCodeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +24,32 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    WellnessScreen()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    val count = 0
+    Text(
+        text = "you've had $count glasses",
+        modifier = modifier.padding(16.dp)
+    )
+
+}
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreViewWellnessScreen() {
+    Surface {
+        WellnessScreen()
     }
 }
